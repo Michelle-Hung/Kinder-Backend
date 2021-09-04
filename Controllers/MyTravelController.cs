cbe683629b13d0b2e48366ac5ae09106b7175cbf
Michelle Hung <chialinghung74@gmail.com>
2021-09-04 23:54:26 +0800

    add pictureName property

--- Controllers/MyTravelController.cs
+++ Controllers/MyTravelController.cs
@@ -19,6 +19,7 @@ namespace Kinder_Backend.Controllers
                 //Context = "水老街分成外側靠淡水河岸的部分(金色水岸步道)與內側的老街，四處可見此區著名的古早味現烤蛋糕、烤魷魚、阿婆鐵蛋、魚酥、巨無霸冰淇淋還有淡水魚丸等等，都是來到此地必吃不可的美食。 從淡水捷運站走出來右轉，中正路及延伸的重建街、清水街一帶，就是鼎鼎大名的淡水老街。 ... 傍晚時，在金色水岸步道還可欣賞夕陽落入海面的美景。",
                 //CreatedOn = DateTime.Now,
                 //ModifiedOn = DateTime.Now
+                PictureName = "Wuling.jpeg"
             },
 
             new TravelList{
@@ -30,6 +31,7 @@ namespace Kinder_Backend.Controllers
                 // Context = "The Twelve Apostles is a collection of limestone stacks off the shore of Port Campbell National Park, by the Great Ocean Road in Victoria, Australia. Their proximity to one another has made the site a popular tourist attraction. Seven of the original eight stacks remain standing at the Twelve Apostles viewpoint, after one collapsed in July 2005.[1] Though the view from the promontory by the Twelve Apostles never included twelve stacks, additional stacks—not considered part of the Apostles group—are located to the west within the national park.[2]",
                 // CreatedOn = DateTime.Now,
                 // ModifiedOn = DateTime.Now
+                PictureName = "Wuling.jpeg"
             },new TravelList{
                 Id = 3,
                 Attraction = "The Twelve Apostles",
@@ -39,6 +41,7 @@ namespace Kinder_Backend.Controllers
                 // Context = "The Twelve Apostles is a collection of limestone stacks off the shore of Port Campbell National Park, by the Great Ocean Road in Victoria, Australia. Their proximity to one another has made the site a popular tourist attraction. Seven of the original eight stacks remain standing at the Twelve Apostles viewpoint, after one collapsed in July 2005.[1] Though the view from the promontory by the Twelve Apostles never included twelve stacks, additional stacks—not considered part of the Apostles group—are located to the west within the national park.[2]",
                 // CreatedOn = DateTime.Now,
                 // ModifiedOn = DateTime.Now
+                PictureName = "Wuling.jpeg"
             },new TravelList{
                 Id = 4,
                 Attraction = "The Twelve Apostles",
@@ -48,6 +51,7 @@ namespace Kinder_Backend.Controllers
                 // Context = "The Twelve Apostles is a collection of limestone stacks off the shore of Port Campbell National Park, by the Great Ocean Road in Victoria, Australia. Their proximity to one another has made the site a popular tourist attraction. Seven of the original eight stacks remain standing at the Twelve Apostles viewpoint, after one collapsed in July 2005.[1] Though the view from the promontory by the Twelve Apostles never included twelve stacks, additional stacks—not considered part of the Apostles group—are located to the west within the national park.[2]",
                 // CreatedOn = DateTime.Now,
                 // ModifiedOn = DateTime.Now
+                PictureName = "Wuling.jpeg"
             },new TravelList{
                 Id = 5,
                 Attraction = "The Twelve Apostles",
@@ -57,6 +61,7 @@ namespace Kinder_Backend.Controllers
                 // Context = "The Twelve Apostles is a collection of limestone stacks off the shore of Port Campbell National Park, by the Great Ocean Road in Victoria, Australia. Their proximity to one another has made the site a popular tourist attraction. Seven of the original eight stacks remain standing at the Twelve Apostles viewpoint, after one collapsed in July 2005.[1] Though the view from the promontory by the Twelve Apostles never included twelve stacks, additional stacks—not considered part of the Apostles group—are located to the west within the national park.[2]",
                 // CreatedOn = DateTime.Now,
                 // ModifiedOn = DateTime.Now
+                PictureName = "Wuling.jpeg"
             },new TravelList{
                 Id = 6,
                 Attraction = "The Twelve Apostles",
@@ -66,6 +71,7 @@ namespace Kinder_Backend.Controllers
                 // Context = "The Twelve Apostles is a collection of limestone stacks off the shore of Port Campbell National Park, by the Great Ocean Road in Victoria, Australia. Their proximity to one another has made the site a popular tourist attraction. Seven of the original eight stacks remain standing at the Twelve Apostles viewpoint, after one collapsed in July 2005.[1] Though the view from the promontory by the Twelve Apostles never included twelve stacks, additional stacks—not considered part of the Apostles group—are located to the west within the national park.[2]",
                 // CreatedOn = DateTime.Now,
                 // ModifiedOn = DateTime.Now
+                PictureName = "Wuling.jpeg"
             },
         };
 
--- Models/Travel.cs
+++ Models/Travel.cs
@@ -8,9 +8,10 @@ namespace Kinder_Backend.Models
         public string Attraction { get; set; }
         // public string Address { get; set; }
         // public string Context { get; set; }
-        public DateTime StartDate { get; internal set; }
+        public DateTime StartDate { get;  set; }
         // public DateTime EndDate { get; internal set; }
         // public DateTime CreatedOn { get; internal set; }
         // public DateTime ModifiedOn { get; internal set; }
+        public string PictureName {get; set;}
     }
 }
